d1573c94744cae1c9f7e2f5f1d568dd961c5c2ef
TitidTerbang <yudisbaek@gmail.com>
2023-05-13 16:26:28 +0700

    image downloader

--- main.py
+++ main.py
@@ -1,16 +1,31 @@
 from bs4 import BeautifulSoup
 import requests
 
-website = 'https://subslikescript.com/movie/American_Psycho-144084'
+website = 'https://nsfwx.pics/4650.html'
 result = requests.get(website)
 content = result.text
 
 soup = BeautifulSoup(content, 'lxml')
-# print(soup.prettify())
+# image = soup.img['src']
+images = soup.find_all('img')
+# print(images)
 
-box = soup.find('article', class_= 'main-article')
+imagesrc = []
+for image in images:
+    imagesrc.append(image['src'])
+print(imagesrc)
 
-title = soup.find('h1').get_text()
-transcript = box.find('div', class_= 'full-script').get_text(strip=True, separator='\n')
-print(title)
-print(transcript)
+for image in imagesrc:
+    webs = requests.get(image)
+    filename = 'bro/' + image.split('/')[-1]
+    open(filename, 'wb').write(webs.content)
+
+# for image in images:
+#     webs = requests.get(image)
+#     open('images/' + image.split('/')[-1], 'wb').write(webs.content)
+# box = soup.find('article', class_= 'main-article')
+
+# title = soup.find('h1').get_text()
+# transcript = box.find('div', class_= 'full-script').get_text(strip=True, separator='\n')
+# print(title)
+# print(transcript)
