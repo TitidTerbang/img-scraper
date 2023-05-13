ee0f1fea84d7f39964003d344c980b468d4615d4
TitidTerbang <yudisbaek@gmail.com>
2023-05-13 19:17:58 +0700

    final

--- main.py
+++ main.py
@@ -7,28 +7,22 @@ result = requests.get(website)
 content = result.text
 
 soup = BeautifulSoup(content, 'lxml')
-# image = soup.img['src']
 images = soup.find_all('a')
-# print(images)
 
 imagesrc = []
 for image in images:
     imagesrc.append(image['href'])
-# print(imagesrc)
 
 filterimg = [filt for filt in imagesrc if 'https://imagehaha.com/' in filt]
-print(filterimg)
-# for image in imagesrc:
-#     webs = requests.get(image)
-#     filename = 'bro/' + image.split('/')[-1]
-#     open(filename, 'wb').write(webs.content)
+filterimg_str = '\n'.join(filterimg)
+linetunnel = filterimg_str.split()
 
-# for image in images:
-#     webs = requests.get(image)
-#     open('images/' + image.split('/')[-1], 'wb').write(webs.content)
-# box = soup.find('article', class_= 'main-article')
+for image in linetunnel:
+    site = requests.get(image)
+    cont = site.text
+    soups = BeautifulSoup(cont, 'lxml')
+    tunnelimage = soups.img['src']
 
-# title = soup.find('h1').get_text()
-# transcript = box.find('div', class_= 'full-script').get_text(strip=True, separator='\n')
-# print(title)
-# print(transcript)
+    webs = requests.get(tunnelimage)
+    filename = 'bro/' + tunnelimage.split('/')[-1]
+    open(filename, 'wb').write(webs.content)
