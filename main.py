0a2fbcd4924cd5dda336b5174f3b0a9397eab1ad
TitidTerbang <yudisbaek@gmail.com>
2023-05-27 22:11:38 +0700

    add status code

--- main.py
+++ main.py
@@ -4,27 +4,30 @@ import requests
 # the website
 website = ''
 result = requests.get(website)
-print(result.status_code)
-content = result.text
 
-soup = BeautifulSoup(content, 'lxml')
-images = soup.find_all('a')
-print(images)
-imagesrc = []
-for image in images:
-    imagesrc.append(image['href'])
+if result.status_code == 200:
+    content = result.text
 
-#filter the source
-filterimg = [filt for filt in imagesrc if 'https://imagehaha.com/' in filt]
-filterimg_str = '\n'.join(filterimg)
-linetunnel = filterimg_str.split()
+    soup = BeautifulSoup(content, 'lxml')
+    images = soup.find_all('a')
 
-for image in linetunnel:
-    site = requests.get(image)
-    cont = site.text
-    soups = BeautifulSoup(cont, 'lxml')
-    tunnelimage = soups.img['src']
+    imagesrc = []
+    for image in images:
+        imagesrc.append(image['href'])
+    # filter the source and get the higher image
+    filterimg = [filt for filt in imagesrc if 'https://images-na.ssl' in filt]
+    filterimg_str = '\n'.join(filterimg)
+    linetunnel = filterimg_str.split()
 
-    webs = requests.get(tunnelimage)
-    filename = 'bro/' + tunnelimage.split('/')[-1]
-    open(filename, 'wb').write(webs.content)
+    for image in linetunnel:
+        site = requests.get(image)
+        cont = site.text
+        soups = BeautifulSoup(cont, 'lxml')
+        tunnelimage = soups.img['src']
+
+        webs = requests.get(tunnelimage)
+        filename = 'bro/' + tunnelimage.split('/')[-1]
+        open(filename, 'wb').write(webs.content)
+
+else:
+    print('Error')
